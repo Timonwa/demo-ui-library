6e6032bf3ad5810307f87837657fee269376d8aa
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-05-05 23:10:33 +0100

    Deleted storybook demo files and added my own

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: "Marbella/Button",
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = (args) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Primary.args = {
+  primary: true,
+  disabled: false,
+  text: "Primary",
+};
+
+export const Secondary: Story = (args) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Secondary.args = {
+  primary: false,
+  disabled: false,
+  text: "Secondary",
+};
+
+export const Disabled: Story = (args) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Disabled.args = {
+  primary: false,
+  disabled: true,
+  text: "Disabled",
+};
+
+export const Small: Story = (args) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Small.args = {
+  primary: true,
+  disabled: false,
+  size: "small",
+  text: "Small",
+};
+
+export const Medium: Story = (args) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Medium.args = {
+  primary: true,
+  disabled: false,
+  size: "medium",
+  text: "Medium",
+};
+
+export const Large: Story = (args) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Large.args = {
+  primary: true,
+  disabled: false,
+  size: "large",
+  text: "Large",
+};
---
+++ src/components/Input.stories.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import Input from "./Input";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  title: "Marbella/InputField",
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const Primary: Story = (args) => (
+  <Input data-testId="InputField-id" {...args} />
+);
+Primary.args = {
+  error: false,
+  disabled: false,
+  label: "Primary",
+};
+
+export const Success: Story = (args) => (
+  <Input data-testId="InputField-id" {...args} />
+);
+Success.args = {
+  error: false,
+  success: true,
+  disabled: false,
+  label: "Success",
+};
+
+export const Error: Story = (args) => (
+  <Input data-testId="InputField-id" {...args} />
+);
+Error.args = {
+  error: true,
+  disabled: false,
+  message: "Error",
+};
+
+export const Disabled: Story = (args) => (
+  <Input data-testId="InputField-id" {...args} />
+);
+Disabled.args = {
+  disabled: true,
+  label: "Disabled",
+};
