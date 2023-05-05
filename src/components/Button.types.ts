d64d7e98008beec187beb118c0bf3eeb81090cd6
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-05-05 22:52:07 +0100

    at the point of creating our first storybook

---
+++ src/components/Button.types.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+export interface ButtonProps {
+  text?: string;
+  primary?: boolean;
+  disabled?: boolean;
+  size?: "small" | "medium" | "large";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
