48c237ba1717774287be009767a5d777581b33e4
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-05-06 01:38:02 +0100

    new dist folder

---
+++ dist/cjs/types/components/Input/Input.types.d.ts
@@ -0,0 +1,11 @@
+import { ChangeEventHandler } from "react";
+export interface InputProps {
+    id?: string;
+    label?: string;
+    error?: boolean;
+    message?: string;
+    success?: boolean;
+    disabled?: boolean;
+    placeholder?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+}
