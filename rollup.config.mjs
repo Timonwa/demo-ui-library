7c0f66cf95e5a0d9da10a6d91c99cf833c15caf0
Andrey Litvinov <drune.email@gmail.com>
2023-05-17 22:26:21 +0300

    stable changes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,10 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import packageJson from "./package.json" assert { type: 'json' };
 
-const packageJson = require("./package.json");
 
 export default [
   {
@@ -25,15 +24,8 @@ export default [
     plugins: [
       peerDepsExternal(),
       resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-    ],
-    external: ["react", "react-dom", "styled-components"],
-  },
-  {
-    input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
-    plugins: [dts.default()],
+      // commonjs(),
+      typescript(),
+    ]
   },
 ];
